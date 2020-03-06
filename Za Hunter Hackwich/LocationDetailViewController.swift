6e93cd704e8ba5700407eaf94086670b4dc49927
Bilal Baig <bbaig@bsd220.org>
2020-03-06 10:19:25 -0600

    Added information buttons to annotations

---
+++ Za Hunter Hackwich/LocationDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationDetailViewController.swift
+//  Za Hunter Hackwich
+//
+//  Created by Bilal Baig on 3/4/20.
+//  Copyright © 2020 Bilal Baig. All rights reserved.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
